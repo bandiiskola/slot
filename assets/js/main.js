59887cd880b930ebad176ee528e2bb18a70f4e80
bandiiskola <nandor.bere@gmail.com>
2023-10-10 11:25:28 +0200

    porget

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 var t = [{
-        sorrend: [0, 0, 0]
+        sorrend: [0, 0, 0, 0]
     },
     {
-        sorrend: [0, 0, 0]
+        sorrend: [0, 0, 0, 0]
     },
     {
-        sorrend: [0, 0, 0]
+        sorrend: [0, 0, 0, 0]
     }
 ]
 
@@ -20,10 +20,26 @@ function Berak() {
 
 function Rajzol() {
     for (let i = 0; i < t.length; i++) {
+        var szorzo=1
         for (y of t[i].sorrend) {
-            document.getElementById("s"+(i+1)).innerHTML+="<img class='kiskep' src='assets/images/"+y+".png'>"
+            document.getElementById("s"+(i+1)).innerHTML+="<img class='kiskep' src='assets/images/"+y+".png' style='top: "+(160*szorzo)+"px'>"
+            szorzo++
         }
     }
 }
 
 Berak()
+
+function porget() {
+    document.getElementById("forgat").disabled=true
+
+    var k = getElementsByClassName("kiskep")
+
+    for (let x of k)
+    {
+        x.style.mar
+    }
+
+
+    //document.getElementById("forgat").disabled=false
+}
