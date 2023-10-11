b9cf73be63c23797f5367be03d0ed65aa211ef3a
bandiiskola <nandor.bere@gmail.com>
2023-10-11 11:10:53 +0200

    Jo

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,9 +31,26 @@ Berak()
 function porget() {
     document.getElementById("forgat").disabled=true
 
+    mozdit()
+}
+
+function mozdit() {
     document.getElementById("s1").getElementsByClassName("kiskep")[0].style.marginTop="-160px"
     document.getElementById("s2").getElementsByClassName("kiskep")[0].style.marginTop="-160px"
     document.getElementById("s3").getElementsByClassName("kiskep")[0].style.marginTop="-160px"
+    document.getElementById("s1").getElementsByClassName("kiskep")[0].remove
+    document.getElementById("s2").getElementsByClassName("kiskep")[0].remove
+    document.getElementById("s3").getElementsByClassName("kiskep")[0].remove
 
-    //document.getElementById("forgat").disabled=false
+    for (var i=0;i<t.length;i++)
+    {
+        t[i].sorrend.shift()
+        t[i].sorrend.push(Math.floor(Math.random()*7)+1)
+        for (y of t[i].sorrend) {
+            document.getElementById("s"+(i+1)).innerHTML+="<img class='kiskep' src='assets/images/"+y+".png'>"
+        }
+    }
+
+    Rajzol()
+    document.getElementById("forgat").disabled=false
 }
